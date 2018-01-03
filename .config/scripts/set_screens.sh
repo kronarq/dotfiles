5b45da9a8d1e3ce833b109f4e0e735881a226f53
Joshua Hansen <kronarq@gmail.com>
2018-01-02 18:35:16 -0700

    Add set_screens script that sets screen resolution/position via nvidia-settings

---
+++ .config/scripts/set_screens.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+nvidia-settings --assign "CurrentMetaMode=DPY-6: 2560x1440_144 @2560x1440 +2560+1440 {ViewPortIn=2560x1440, ViewPortOut=2560x1440+0+0}, DPY-1: 2560x1440_60 @2560x1440 +2560+0 {ViewPortIn=2560x1440, DViewPortOut=2560x1440+0+0}, DPY-2: 2560x1440_144 @2560x1440 +5120+1440 {ViewPortIn=2560x1440, ViewPortOut=2560x1440+0+0}, DPY-4: 2560x1440_144 @2560x1440 +0+1440 {ViewPortIn=2560x1440, ViewPortOut=2560x1440+0+0}"
+
